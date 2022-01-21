7b89930e01061eb0015c246324ab7a3b0685c6a2
Wayne <w702897028970289@gmail.com>
2022-01-21 12:22:05 +0800

    新增寶可夢圖片

--- app/src/main/java/com/setDDG/glideFunction/GlideFunctionFragment.kt
+++ app/src/main/java/com/setDDG/glideFunction/GlideFunctionFragment.kt
@@ -1,10 +1,10 @@
 package com.setDDG.glideFunction
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.rockex6.practiceappfoundation.R
 import com.setDDG.util.loadImage
 import kotlinx.android.synthetic.main.fragment_glide_function.*
@@ -13,23 +13,25 @@ import kotlinx.android.synthetic.main.fragment_glide_function.*
 class GlideFunctionFragment : Fragment() {
 
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_glide_function, container, false)
     }
 
     companion object {
-       fun newInstance() = GlideFunctionFragment()
+        fun newInstance() = GlideFunctionFragment()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        catImage.loadImage(this,"https://cataas.com/cat")
+
+        pokemonImageView.loadImage(this,
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${(1..151).random()}.png")
+        randomButton.setOnClickListener {
+            pokemonImageView.loadImage(this,
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${(1..151).random()}.png")
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        catImage.loadImage(this,"https://cataas.com/cat")
-    }
+
 }
